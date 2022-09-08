818962e46e52b4c5d218d4000dd18eea726e40a1
Afsan Saeed <afsansaeed2017@gmail.com>
2022-09-08 20:48:03 +0600

    Crud updated

--- base/urls.py
+++ base/urls.py
@@ -4,7 +4,10 @@ from .import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('login/', views.login_page, name='login_page'),
+    path('logout/', views.logout_user, name='logout_user'),
     path('room/<str:pk>/', views.room, name='room'),
     path('create-room/', views.create_room, name='create-room'),
     path('update-room/<str:pk>', views.update_room, name='update-room'),
+    path('delete-room/<str:pk>', views.delete_room, name='delete-room'),
     ]
